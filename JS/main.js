3be8d183b82fab746b6617b41fd81c9999938757
JennyLamego2022 <jennylamego@hotmail.com>
2022-11-30 17:46:21 -0300

    Criando efeito html para o efeito parallax

---
+++ JS/main.js
@@ -0,0 +1,54 @@
+// function insert(){
+//    console.log('Olá mundo');
+// }
+
+// let num = document.getElementById('input-conta').innerHTML;
+
+
+// var inputValor = "";
+// var vconta = input.value;
+
+// var nPessoas = document.querySelector('#nPessoas');
+
+// var vGorjeta = document.getElementById();
+// var tGorjeta = document.getElementById();
+
+// var text = input.value;
+// var text = input.value;
+// console.log(conta);
+
+
+function insert(){
+    var percent = document.getElementById("percent5").value;
+    var conta = document.getElementById('input-conta').value;
+    console.log(insert);
+
+    // document.getElementById('total-gorjeta').innerHTML = inputValor;
+
+    document.getElementById('total-gorjeta').value =
+        (conta / 100)* 5;
+    }
+
+
+
+// const contaInput = document.querySelector(".input-conta");
+
+// contaInput.addEventListener("input", contaInputFun);
+
+// contaInput.value = "0.0";
+
+// let contaValue = 0.0;
+
+// function contaInputFun(){
+//     contaValue = parseFloat(contaInput.value);
+//     console.log(contaValue);
+
+// }
+
+// var percent = document.getElementById("percent5").value;
+   
+// function insert(){
+//     var percent = document.getElementById('percent5').value;
+//     var conta = document.getElementById('input-conta').value;
+//   document.getElementById('total-gorjeta').value = (conta / 100)* percent;
+//     }
